f3dd08e77d3a0528c436a93e148af3ca71ebab6a
eaapen <eaapen@INF30>
2024-05-21 14:37:36 -0600

    Ejercicio de consumir SOAP BANGUAT

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientSOAP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Banguat.TipoCambio cliente = new Banguat.TipoCambio();
+            var resultado = cliente.TipoCambioDia();
+            Console.WriteLine(resultado.CambioDolar.First().fecha);
+            Console.WriteLine(resultado.CambioDolar.First().referencia);
+
+            Console.ReadLine();
+        }
+    }
+}
